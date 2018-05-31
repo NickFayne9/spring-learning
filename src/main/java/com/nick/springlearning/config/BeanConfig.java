eeba44c72be9d8d596d8e4efac33b5f967f6670b
Nick Fayne <nickfayne@aliyun.com>
2018-05-31 20:57:25 +0800

    完成 xml 方式配置

---
+++ src/main/java/com/nick/springlearning/config/BeanConfig.java
@@ -0,0 +1,13 @@
+package com.nick.springlearning.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Java Config 方式（注解配置方式）配置类
+ * @author NickFayne 2018-05-31 20:55
+ */
+@Configuration
+public class BeanConfig {
+
+    
+}
---
+++ src/test/java/com/nick/springlearning/AnnotationConfigTest.java
@@ -0,0 +1,8 @@
+package com.nick.springlearning;
+
+/**
+ * 注解配置方式测试类
+ * @author NickFayne 2018-05-31 20:55
+ */
+public class AnnotationConfigTest {
+}
