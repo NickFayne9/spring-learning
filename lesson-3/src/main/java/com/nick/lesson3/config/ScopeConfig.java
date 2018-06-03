de4964cd73a8ff84a93bbb52cae550cf10eb5846
Nick Fayne <nickfayne@aliyun.com>
2018-06-03 08:58:07 +0800

    新增 lesson-3

---
+++ lesson-3/src/main/java/com/nick/lesson3/config/ScopeConfig.java
@@ -0,0 +1,12 @@
+package com.nick.lesson3.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author NickFayne 2018-06-03 8:53
+ */
+@Configuration
+public class ScopeConfig {
+
+
+}
---
+++ lesson-3/src/main/java/com/nick/lesson3/model/Person.java
@@ -0,0 +1,44 @@
+package com.nick.lesson3.model;
+
+/**
+ * 模型类 人
+ * @author NickFayne 2018-06-03 8:55
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        System.out.println("调用了 Person 默认的构造方法...");
+    }
+
+    public Person(String name, int age) {
+        System.out.println("调用了 Person 两个参数的构造方法...");
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
