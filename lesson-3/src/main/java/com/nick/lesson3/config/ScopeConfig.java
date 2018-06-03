94f6a8b0f98a78215cd8688e45d9a4842254cfb4
Nick Fayne <nickfayne@aliyun.com>
2018-06-03 09:06:44 +0800

    提交测试类

--- lesson-3/src/main/java/com/nick/lesson3/config/ScopeConfig.java
+++ lesson-3/src/main/java/com/nick/lesson3/config/ScopeConfig.java
@@ -1,12 +1,20 @@
 package com.nick.lesson3.config;
 
+import com.nick.lesson3.model.Person;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
+ * Spring 配置类
  * @author NickFayne 2018-06-03 8:53
  */
 @Configuration
 public class ScopeConfig {
 
-
+    //@Scope("prototype")
+    @Bean
+    public Person person(){
+        return new Person();
+    }
 }
---
+++ lesson-3/src/test/java/com/nick/lesson3/ScopeTest.java
@@ -0,0 +1,24 @@
+package com.nick.lesson3;
+
+import com.nick.lesson3.config.ScopeConfig;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * 测试 Scope 类
+ * @author NickFayne 2018-06-03 8:59
+ */
+public class ScopeTest {
+
+    @Test
+    public void testScope1(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfig.class);
+        System.out.println(applicationContext.getBean("person").hashCode());
+        System.out.println(applicationContext.getBean("person").hashCode());
+        String[] beans = applicationContext.getBeanDefinitionNames();
+        for(String bean : beans){
+            System.out.println(bean);
+        }
+    }
+}
