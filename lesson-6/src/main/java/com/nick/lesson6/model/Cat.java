c41a650f819daf455580ac75b2a195931a745123
Nick Fayne <nickfayne@aliyun.com>
2018-06-04 21:12:04 +0800

    完善 init destroy

--- lesson-6/src/main/java/com/nick/lesson6/model/Cat.java
+++ lesson-6/src/main/java/com/nick/lesson6/model/Cat.java
@@ -3,6 +3,9 @@ package com.nick.lesson6.model;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * 模型类 猫
  * @author NickFayne 2018-06-04 20:33
@@ -22,10 +25,20 @@ public class Cat implements InitializingBean, DisposableBean {
     }
 
     public void afterPropertiesSet() throws Exception {
-        System.out.println("猫的 InitializingBean.afterPropertiesSet 销毁方法");
+        System.out.println("猫的 InitializingBean.afterPropertiesSet 初始化方法");
     }
 
     public void destroy() throws Exception {
         System.out.println("猫的 DisposableBean.destroy 销毁方法");
+    }
+
+    @PostConstruct
+    public void postConstructor(){
+        System.out.println("猫的 postConstructor 初始化方法");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("猫的 preDestroy 销毁方法");
     }
 }
